b7afe5a68e0403ad86b529437a2d9f2b45301bf9
Carlos Oliveira <c.oliveiradev@gmail.com>
2025-11-04 08:25:38 -0300

    feat: add update, delete and search by cpf

--- routes/routes.go
+++ routes/routes.go
@@ -10,6 +10,9 @@ func HandleRequests() {
 	r.GET("/alunos", controllers.RetornaTodosOsAlunos)
 	r.GET("/:nome", controllers.Saudacao)
 	r.GET("/alunos/:id", controllers.BuscaAlunoPorID)
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
 	r.POST("/alunos", controllers.CriaNovoAluno)
+	r.PATCH("/alunos/:id", controllers.EditaAluno)
+	r.DELETE("/alunos/:id", controllers.DeletaAluno)
 	r.Run()
 }
