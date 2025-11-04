04b1a879b46742e759b83f5c94eed33ff6e46213
Carlos Oliveira <c.oliveiradev@gmail.com>
2025-11-04 07:56:14 -0300

    feat: Add get all and get by id

--- main.go
+++ main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"github.com/DevCarlosOli/gin-api-rest/database"
-	"github.com/DevCarlosOli/gin-api-rest/models"
 	"github.com/DevCarlosOli/gin-api-rest/routes"
 )
 
 func main() {
 	database.ConectaComBancoDeDados()
-	models.Alunos = []models.Aluno{
-		{Nome: "Carlos Filho", CPF: "00000000000", RG: "123456"},
-		{Nome: "Aline Ferreira", CPF: "99999999999", RG: "654321"},
-	}
 	routes.HandleRequests()
 }
