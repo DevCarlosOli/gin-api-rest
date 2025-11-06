638ed3b316224aef33e9f1ebf2f12b0a536cae19
Carlos Oliveira <c.oliveiradev@gmail.com>
2025-11-05 22:29:01 -0300

    feat: Adicionando novos testes de validação

--- main_test.go
+++ main_test.go
@@ -7,13 +7,29 @@ import (
 	"testing"
 
 	"github.com/DevCarlosOli/gin-api-rest/controllers"
+	"github.com/DevCarlosOli/gin-api-rest/database"
+	"github.com/DevCarlosOli/gin-api-rest/models"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
+var ID int
+
 func SetupDasRotasDeTeste() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
 	rotas := gin.Default()
 	return rotas
+}
+
+func CriaAlunoMock() {
+	aluno := models.Aluno{Nome: "Fulano Teste", CPF: "01234567891", RG: "123456789"}
+	database.DB.Create(&aluno)
+	ID = int(aluno.ID)
+}
+
+func DeletaAlunoMock() {
+	var aluno models.Aluno
+	database.DB.Delete(&aluno, ID)
 }
 
 func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
@@ -26,4 +42,28 @@ func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 	mockDaResponse := `{"API diz:":"E aí carlos, tudo beleza?"}`
 	responseBody, _ := ioutil.ReadAll(response.Body)
 	assert.Equal(t, mockDaResponse, string(responseBody))
+}
+
+func TestListandoTodosOsAlunosHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos", controllers.RetornaTodosOsAlunos)
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, req)
+	assert.Equal(t, http.StatusOK, response.Code)
+}
+
+func TestBuscaAlunoPorCPFHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
+	req, _ := http.NewRequest("GET", "/alunos/cpf/01234567891", nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, req)
+	assert.Equal(t, http.StatusOK, response.Code)
 }
